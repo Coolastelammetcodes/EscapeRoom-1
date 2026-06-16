a5d49355a89c3d2df6ad08b013174262820b70fb
Coolastelammetcodes <Martinlam36@gmail.com>
2026-06-16 13:52:51 +0200

    Added code for time measurment in site.js and also linked js-file to _Layout.cshtml

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,9 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿document.addEventListener("DOMContentLoaded", function () {
 
-// Write your JavaScript code.
+    if (!localStorage.getItem('escapeStartTime')) {
+        localStorage.setItem('escapeStartTime', Date.now());
+    }
+
+    var t = localStorage.getItem('escapeStartTime');
+    window.location.href = 'http://room2.runasp.net/?t=' + t;
+});
