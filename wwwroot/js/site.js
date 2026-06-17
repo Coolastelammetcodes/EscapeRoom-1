e7ffac88286f9b4588e9400f39ef2a40540f4b71
Elisa <st.elisa21@gmail.com>
2026-06-17 11:03:14 +0200

    changed logic for time counting

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,27 +1,25 @@
 ﻿document.addEventListener("DOMContentLoaded", function () {
+  const goToRoom2 = document.getElementById("goToRoom2");
 
-    const goToRoom2 = document.getElementById("goToRoom2");
+  if (!goToRoom2) {
+    console.log("Room 2 button NOT found (isSolved=false?)");
+    return;
+  }
 
-    if (!goToRoom2) {
-        console.log("Room 2 button NOT found (isSolved=false?)");
-        return;
-    }
+  goToRoom2.addEventListener("click", function () {
+    const hintEl = document.getElementById("hintCount");
 
-    goToRoom2.addEventListener("click", function () {
+    const hintCount = Number(hintEl?.value || 0);
 
-        const hintEl = document.getElementById("hintCount");
+    const t = Date.now() - hintCount * 15000;
 
-        const hintCount = Number(hintEl?.value || 0);
-
-        const t = Date.now() + (hintCount * 15000);
-
-        //console.log för att kolla att korrekt tid skickas vidare 
-        console.log({
-            hintCount,
-            now: Date.now(),
-            t
-        });
-
-        window.location.href = "http://room2.runasp.net/?t=" + t;
+    //console.log för att kolla att korrekt tid skickas vidare
+    console.log({
+      hintCount,
+      now: Date.now(),
+      t,
     });
+
+    window.location.href = "http://room2.runasp.net/?t=" + t;
+  });
 });
